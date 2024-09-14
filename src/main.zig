b0139d5a500719247a6a427141aa3c73e060d2c3
AndreasHefti <anhefti@yahoo.de>
2024-09-14 14:15:22 +0200

    build

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,5 @@
 const std = @import("std");
-const firefly = @import("inari/firefly/firefly.zig");
+pub const firefly = @import("inari/firefly/firefly.zig");
 
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
