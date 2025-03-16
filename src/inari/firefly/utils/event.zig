0bcbb73b03dfe4673d50ab802bcefed6d916aac7
AndreasHefti <anhefti@yahoo.de>
2025-03-16 10:54:40 +0100

    events: fixed concurrent event list modifications

--- src/inari/firefly/utils/event.zig
+++ src/inari/firefly/utils/event.zig
@@ -44,7 +44,14 @@ pub fn EventDispatch(comptime E: type) type {
 
         pub fn notify(self: *Self, event: E) void {
             for (0..self.listeners.items.len) |i| {
-                self.listeners.items[i](event);
+                // Note: since a listener call can potentially process a event list modification this prevents
+                //       unintentional array index out of bounds while iteration on old len.
+                //       This solution is not ideal since the behavior is not well defined for a listener call that
+                //       modifies the event listener list itself but is prevents errors at least.
+                //       Another approach would be to copy the list of listener pointers always before and iterate
+                //       over the copy then. But this would lead to less performance and needs more memory alloc/dealloc
+                if (i < self.listeners.items.len)
+                    self.listeners.items[i](event);
             }
         }
     };
