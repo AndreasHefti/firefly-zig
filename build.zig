176be3df78bacb006ce674588e04b9c20e945db7
AndreasHefti <anhefti@yahoo.de>
2025-05-08 20:21:01 +0200

    release small

--- build.zig
+++ build.zig
@@ -13,7 +13,7 @@ pub fn build(b: *std.Build) void {
     // Standard optimization options allow the person running `zig build` to select
     // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall. Here we do not
     // set a preferred release mode, allowing the user to decide how to optimize.
-    const optimize = std.builtin.OptimizeMode.Debug;
+    const optimize = std.builtin.OptimizeMode.ReleaseSmall;
 
     // define the raylib dependency
     const raylib_dep = b.dependency("raylib", .{
